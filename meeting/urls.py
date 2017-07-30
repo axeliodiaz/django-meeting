b1eae10f4ecfe31795da58d60c380829e9eae7dc
Axelio <diaz.axelio@gmail.com>
2017-07-30 10:52:07 -0400

    Añadiendo y reordenando urls para edicion y creacion de salas

--- meeting/urls.py
+++ meeting/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import RoomListView
+from .views import RoomListView, RoomEditView, RoomCreateView
 
 
 urlpatterns = [
-    url(r'^', RoomListView.as_view(), name='meeting_dashboard'),
+    url(r'^(?P<id>\d+)/change/', RoomEditView.as_view(), name='meeting_edit'),
+    url(r'^add/', RoomCreateView.as_view(), name='meeting_add'),
+    url(r'^', RoomListView.as_view(), name='meeting_list'),
 ]
